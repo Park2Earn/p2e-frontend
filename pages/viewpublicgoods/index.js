8388f2529e8cdd9927a5e4899b259b7f395cba85
rimatik <slaven@rimatik.hr>
2022-07-24 12:30:53 +0200

    added fetching and table to public goods

--- pages/viewpublicgoods/index.js
+++ pages/viewpublicgoods/index.js
@@ -1,241 +1,311 @@
+import { useState, useEffect } from "react";
 
-import { useState, useEffect } from 'react'
-
-import { Paper, Grid, Button,TextField } from '@material-ui/core'
+import { Paper, Grid, Button, TextField } from "@material-ui/core";
 // import { daoContractAddress, nftURI , REACT_APP_ALCHEMY_KEY} from '../../config'
-import { park2EarnContractAddress, REACT_APP_ALCHEMY_KEY } from '../../config'
-import Park2Earn from '../../contracts/Park2Earn.json'
-import { ethers } from 'ethers'
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import Moralis from "moralis"
+import { park2EarnContractAddress, REACT_APP_ALCHEMY_KEY } from "../../config";
+import Park2Earn from "../../contracts/Park2Earn.json";
+import { ethers } from "ethers";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import Moralis from "moralis";
 
-import NavBar from '../../src/navbar'
+import NavBar from "../../src/navbar";
 
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(REACT_APP_ALCHEMY_KEY); 
+const web3 = createAlchemyWeb3(REACT_APP_ALCHEMY_KEY);
 export const Park2EarnContract = new web3.eth.Contract(
-	Park2Earn.abi,
-	park2EarnContractAddress
+  Park2Earn.abi,
+  park2EarnContractAddress
 );
 
-
 export default function Home() {
-    const [txError, setTxError] = useState(null)
-  const [walletError, setWalletError] = useState(null)
-  const [currentAccount, setCurrentAccount] = useState("")
-	const [requestedAccounts, setRequestedAccounts] = useState(false)
-  const [correctNetwork, setCorrectNetwork] = useState(false)
- 
-  const [proposalCount, setProposalCount] = useState(0)
-  const [promotions, setPromotions] = useState(null)
+  const [txError, setTxError] = useState(null);
+  const [walletError, setWalletError] = useState(null);
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [requestedAccounts, setRequestedAccounts] = useState(false);
+  const [correctNetwork, setCorrectNetwork] = useState(false);
 
-
-      // Checks if wallet is connected
-	const checkIfWalletIsConnected = async () => {
-		const { ethereum } = window
-		if (ethereum) {
-			// console.log('Got the ethereum obejct: ', ethereum)
-			const accounts = await ethereum.request({ method: 'eth_accounts' })
-
-			if (accounts.length !== 0) {
-				// console.log('Found authorized Account: ', accounts[0])
-				setCurrentAccount(accounts[0])
-			} else {
-				// console.log('No authorized account found')
-			}
-		} else {
-			setWalletError('Please install MetaMask Wallet.')
-		}
-	}
-
-    // Checks if wallet is connected to the correct network
-	const checkCorrectNetwork = async () => {
-		const { ethereum } = window
-		if (ethereum) {
-			let chainId = await ethereum.request({ method: 'eth_chainId' })
-			// console.log('Connected to chain:' + chainId)
-
-			// const rinkebyChainId = '0x2a'
-
-			// const devChainId = 1337
-      const rinkebyChainId = '0x13881'
-
-			const devChainId = 80001
-			const localhostChainId = `0x${Number(devChainId).toString(16)}`
-
-			if (chainId !== rinkebyChainId && chainId !== localhostChainId) {
-				setCorrectNetwork(false)
-			} else {
-				setCorrectNetwork(true)
-			}
-		} else {
-			setWalletError('Please install MetaMask Wallet.')
-		}
-	}
-
-
-  function walletListener() {
-		const { ethereum } = window
-		if (ethereum) {
-			ethereum.on('accountsChanged', (accounts) => {
-				// Handle the new accounts, or lack thereof.
-				// "accounts" will always be an array, but it can be empty.
-				window.location.reload();
-			});
-			
-			ethereum.on('chainChanged', (chainId) => {
-				// Handle the new chain.
-				// Correctly handling chain changes can be complicated.
-				// We recommend reloading the page unless you have good reason not to.
-				window.location.reload();
-			});
-		} else {
-			setWalletError('Please install MetaMask Wallet.')
-		}
-
-		//   ethereum.on('message', mesg => {
-		// 	  console.log("asd435&&")
-		// 	  console.log(msg)
-		//   })
-
-	  }
+  const [publicGoodsCount, setPublicGoodCount] = useState(0);
+  const [publicGoods, setPublicGoods] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-		checkIfWalletIsConnected()
-		checkCorrectNetwork()
+    async function getAll() {
+      setIsLoading(true);
+      const proposalCount = await Park2EarnContract.methods
+        .getPublicGoodsCount()
+        .call(); // returns int
+      console.log(proposalCount);
+      setPublicGoodCount(proposalCount);
 
-		walletListener()
-	
-	}, [currentAccount])
+      var publicGoodArray = [];
+      for (var i = 1; i <= proposalCount; i++) {
+        const promotion = await Park2EarnContract.methods
+          .getPublicGood(i)
+          .call(); // returns int
+        publicGoodArray.push(promotion);
+      }
+      console.log(publicGoodArray);
+      setPublicGoods(publicGoodArray);
+      setIsLoading(false);
+    }
 
+    getAll();
+  }, []);
 
-    // Calls Metamask to connect wallet on clicking Connect Wallet button
-	const connectWallet = async () => {
-		try {
-			const { ethereum } = window
+  // Checks if wallet is connected
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+    if (ethereum) {
+      // console.log('Got the ethereum obejct: ', ethereum)
+      const accounts = await ethereum.request({ method: "eth_accounts" });
 
-			if (!ethereum) {
-				setWalletError('Please install MetaMask Wallet.')
-				return
-			}
-			let chainId = await ethereum.request({ method: 'eth_chainId' })
+      if (accounts.length !== 0) {
+        // console.log('Found authorized Account: ', accounts[0])
+        setCurrentAccount(accounts[0]);
+      } else {
+        // console.log('No authorized account found')
+      }
+    } else {
+      setWalletError("Please install MetaMask Wallet.");
+    }
+  };
+
+  // Checks if wallet is connected to the correct network
+  const checkCorrectNetwork = async () => {
+    const { ethereum } = window;
+    if (ethereum) {
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      // console.log('Connected to chain:' + chainId)
+
+      // const rinkebyChainId = '0x2a'
+
+      // const devChainId = 1337
+      const rinkebyChainId = "0x13881";
+
+      const devChainId = 80001;
+      const localhostChainId = `0x${Number(devChainId).toString(16)}`;
+
+      if (chainId !== rinkebyChainId && chainId !== localhostChainId) {
+        setCorrectNetwork(false);
+      } else {
+        setCorrectNetwork(true);
+      }
+    } else {
+      setWalletError("Please install MetaMask Wallet.");
+    }
+  };
+
+  function walletListener() {
+    const { ethereum } = window;
+    if (ethereum) {
+      ethereum.on("accountsChanged", (accounts) => {
+        // Handle the new accounts, or lack thereof.
+        // "accounts" will always be an array, but it can be empty.
+        window.location.reload();
+      });
+
+      ethereum.on("chainChanged", (chainId) => {
+        // Handle the new chain.
+        // Correctly handling chain changes can be complicated.
+        // We recommend reloading the page unless you have good reason not to.
+        window.location.reload();
+      });
+    } else {
+      setWalletError("Please install MetaMask Wallet.");
+    }
+
+    //   ethereum.on('message', mesg => {
+    // 	  console.log("asd435&&")
+    // 	  console.log(msg)
+    //   })
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    checkCorrectNetwork();
+
+    walletListener();
+  }, [currentAccount]);
+
+  // Calls Metamask to connect wallet on clicking Connect Wallet button
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        setWalletError("Please install MetaMask Wallet.");
+        return;
+      }
+      let chainId = await ethereum.request({ method: "eth_chainId" });
       // let chainId = await ethereum.request({
       //     method: 'wallet_switchEthereumChain',
       //   params: [{ chainId: '0x13881' }], // '0x3830303031'
       // })
-			// console.log('Connected to chain:' + chainId)
+      // console.log('Connected to chain:' + chainId)
 
-			const rinkebyChainId = '0x13881'
+      const rinkebyChainId = "0x13881";
 
-			const devChainId = 80001
-			const localhostChainId = `0x${Number(devChainId).toString(16)}`
+      const devChainId = 80001;
+      const localhostChainId = `0x${Number(devChainId).toString(16)}`;
 
-      console.log(localhostChainId)
-      console.log(chainId)
-			if (chainId !== rinkebyChainId && chainId !== localhostChainId) {
-				alert('You are not connected to the Polygon Mumbai Testnet!')
-				return
-			}
+      console.log(localhostChainId);
+      console.log(chainId);
+      if (chainId !== rinkebyChainId && chainId !== localhostChainId) {
+        alert("You are not connected to the Polygon Mumbai Testnet!");
+        return;
+      }
 
-			// console.log(requestedAccounts)
-			setRequestedAccounts(true)
+      // console.log(requestedAccounts)
+      setRequestedAccounts(true);
 
-			const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
-			
-			// console.log('Found account', accounts[0])
-			setCurrentAccount(accounts[0])
-		} catch (error) {
-			// console.log('Error connecting to metamask', error)
-		}
-	}
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
 
-  
-  const getProposals = async () => {
+      // console.log('Found account', accounts[0])
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      // console.log('Error connecting to metamask', error)
+    }
+  };
+
+  const getPublicGoods = async () => {
     if (currentAccount != "") {
-			// console.log("getting BORROW  ----- nfts 0")
-            const proposalCount = await Park2EarnContract.methods.getPrivateGoodsCount().call() // returns int
-            console.log(proposalCount)
-            setProposalCount(proposalCount)
+      const proposalCount = await Park2EarnContract.methods
+        .getPublicGoodsCount()
+        .call(); // returns int
+      console.log(proposalCount);
+      setPublicGoodCount(proposalCount);
 
-            var promArray = []
-            for (var i = 0; i < proposalCount; i++) {
-                const promotion = await Park2EarnContract.methods.getPrivateGood(i).call() // returns int
-                promArray.push(promotion)
+      var publicGoodArray = [];
+      for (var i = 1; i <= proposalCount; i++) {
+        const promotion = await Park2EarnContract.methods
+          .getPublicGood(i)
+          .call(); // returns int
+        publicGoodArray.push(promotion);
+      }
+      console.log(publicGoodArray);
+      setPublicGoods(publicGoodArray);
+    }
+  };
 
-            }
-            setPromotions(promArray)
-		}
-  }
+  return (
+    <div>
+      <NavBar />
 
+      <Grid container item xs={12}>
+        <Grid container item xs={3} justifyContent="center"></Grid>
 
-    return (
-        <div>
-            <NavBar/>
-
-        <Grid container item xs={12}>
-            <Grid container item xs={3} justifyContent="center">
-            </Grid>
-
-            <Grid container item xs={6} justify="center">
-                {walletError === null ? (
-                currentAccount === "" ? (
-                    <Button
-                        variant="outlined" disableElevation
-                        style={{ border: '2px solid', height: "50px", width: "100%", margin: "2px", marginTop: "80px", maxWidth: "200px" }}
-                        aria-label="View Code"
-                        disabled={(currentAccount === "" && requestedAccounts)}
-                        onClick={connectWallet}
-
+        <Grid container item xs={6} justifyContent="center">
+          {walletError === null ? (
+            currentAccount === "" ? (
+              <Button
+                variant="outlined"
+                disableElevation
+                style={{
+                  border: "2px solid",
+                  height: "50px",
+                  width: "100%",
+                  margin: "2px",
+                  marginTop: "80px",
+                  maxWidth: "200px",
+                }}
+                aria-label="View Code"
+                disabled={currentAccount === "" && requestedAccounts}
+                onClick={connectWallet}
+              >
+                Connect Wallet
+              </Button>
+            ) : correctNetwork ? (
+              <div style={{ width: "100%" }}>
+                <Grid container item xs={12} justifyContent="center">
+                  <h1
+                    style={{ marginTop: "80px", color: "white", width: "100%" }}
+                  >
+                    Public goods
+                  </h1>
+                </Grid>
+                {isLoading ? (
+                  <div style={{ color: "black" }}>Loading...</div>
+                ) : publicGoods.length > 0 ? (
+                  <div style={{ width: "100%" }}>
+                    <Grid
+                      style={{
+                        border: "1px solid black",
+                        borderRadius: "10px",
+                      }}
                     >
-                        Connect Wallet
-                    </Button>
-                ) : correctNetwork ? (
-                    <div>
-                        <Grid container item xs={12} justify="center">
-                            <Button
-                                variant="outlined" disableElevation
-                                style={{ border: '2px solid', height: "50px", width: "100%", margin: "2px", marginTop: "60px", maxWidth: "200px" }}
-                                aria-label="View Code"
-                                onClick={getProposals}
-                                // disabled={(nftList.length >= 2 || numMinted == 50)}
-                                >
-                                Get All Public Goods
-                            </Button> 
+                      <Grid
+                        item
+                        container
+                        xs={12}
+                        direction="row"
+                        style={{ padding: "5px" }}
+                      >
+                        <Grid
+                          container
+                          item
+                          xs={6}
+                          j
+                          justifyContent="center"
+                          style={{ fontWeight: "bold" }}
+                        >
+                          {"Title"}
                         </Grid>
 
-                        <Grid container item xs={12} justify="center">
-                            <div>
-                                {proposalCount > 0 ? (
-                                    promotions.map()
-                                    // <Grid container item xs={12} justify="center">
-
-                                    //     <Grid container item xs={12} justify="center">
-                                    //         {proposal.description}
-                                    //     </Grid>
-
-                                    //     <Grid container item xs={12} justify="center">
-                                    //         {proposal.description}
-                                    //     </Grid>
-
-                                    // </Grid>
-                                ) : (
-                                    <div></div>
-                                )}
-                            </div>
+                        <Grid
+                          container
+                          item
+                          xs={6}
+                          justifyContent="center"
+                          style={{ fontWeight: "bold" }}
+                        >
+                          {"Description"}
                         </Grid>
-                    </div>
+                      </Grid>
 
-                ) : (
-                    <Paper elevation={0}
-                    style={{width: "100%", margin: "2px", marginTop: "80px", maxWidth: "250px", textAlign: "center"}}
-                    >
-                        Please connect to Polygon Mumbai Testnet
-                    </Paper>
-                )) : (
-                    <div style={{marginTop: "80px"}}>{walletError}</div>
-                )}
-            </Grid>
-            </Grid>
-            </div>
-        )
+                      {publicGoods.map((publicGood, index) => {
+                        return (
+                          <Grid
+                            item
+                            container
+                            xs={12}
+                            key={index}
+                            style={{ padding: "5px" }}
+                          >
+                            <Grid container item xs={6} justifyContent="center">
+                              {publicGood.title}
+                            </Grid>
+
+                            <Grid container item xs={6} justifyContent="center">
+                              {publicGood.description}
+                            </Grid>
+                          </Grid>
+                        );
+                      })}
+                    </Grid>
+                  </div>
+                ) : null}
+              </div>
+            ) : (
+              <Paper
+                elevation={0}
+                style={{
+                  width: "100%",
+                  margin: "2px",
+                  marginTop: "80px",
+                  maxWidth: "250px",
+                  textAlign: "center",
+                }}
+              >
+                Please connect to Polygon Mumbai Testnet
+              </Paper>
+            )
+          ) : (
+            <div style={{ marginTop: "80px" }}>{walletError}</div>
+          )}
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
